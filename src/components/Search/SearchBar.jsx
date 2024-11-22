40b06f625ed2088133d74cfd426e981ee1c2b3db
DVDVALENCIA <dvdvalenciavasquez88@gmail.com>
2024-11-22 00:43:59 -0500

    estilos en descripcion para que al hacer hover halla un zoom

--- src/components/Search/SearchBar.jsx
+++ src/components/Search/SearchBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import "../../styles/SearchBar.css";
 const SearchBar = ({ onSearch }) => {
   const [query, setQuery] = useState("");
   const [category, setCategory] = useState("characters");
@@ -10,12 +10,13 @@ const SearchBar = ({ onSearch }) => {
     onSearch(category, query);
   };
 
+
   return (
-    <div className="search-bar p-4 bg-gray-800 flex flex-col sm:flex-row items-center">
+    <div class="search-bar">
       <select
         value={category}
         onChange={(e) => setCategory(e.target.value)}
-        className="border border-gray-600 bg-gray-700 text-white rounded px-4 py-2 mb-2 sm:mb-0 sm:mr-4"
+        className="select-category"
       >
         <option value="characters">Characters</option>
         <option value="comics">Comics</option>
@@ -28,11 +29,11 @@ const SearchBar = ({ onSearch }) => {
         placeholder="Search..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className="border border-gray-600 bg-gray-700 text-white rounded px-4 py-2 w-full sm:w-auto"
+        class="input-search"
       />
       <button
         onClick={handleSearch}
-        className="bg-green-600 hover:bg-green-700 text-white rounded px-4 py-2 ml-0 sm:ml-4"
+        class="button-search"
       >
         Search
       </button>
@@ -41,3 +42,38 @@ const SearchBar = ({ onSearch }) => {
 };
 
 export default SearchBar;
+
+
+
+
+//   return (
+//     <div className="search-bar p-4 bg-gray-800 flex flex-col sm:flex-row items-center">
+//       <select
+//         value={category}
+//         onChange={(e) => setCategory(e.target.value)}
+//         className="border border-gray-600 bg-gray-700 text-white rounded px-4 py-2 mb-2 sm:mb-0 sm:mr-4"
+//       >
+//         <option value="characters">Characters</option>
+//         <option value="comics">Comics</option>
+//         <option value="events">Events</option>
+//         <option value="series">Series</option>
+//         <option value="stories">Stories</option>
+//       </select>
+//       <input
+//         type="text"
+//         placeholder="Search..."
+//         value={query}
+//         onChange={(e) => setQuery(e.target.value)}
+//         className="border border-gray-600 bg-gray-700 text-white rounded px-4 py-2 w-full sm:w-auto"
+//       />
+//       <button
+//         onClick={handleSearch}
+//         className="bg-green-600 hover:bg-green-700 text-white rounded px-4 py-2 ml-0 sm:ml-4"
+//       >
+//         Search
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default SearchBar;
